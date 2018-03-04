cc1eabc0e6e23975ece86546ff8addd17dc38581
lord-kappa <jan.urbanc4@gmail.com>
2018-03-04 14:04:47 +0100

    css prva stran

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,6 @@ class Book < ActiveRecord::Base
 belongs_to :user
 has_many :comments
 
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+	has_attached_file :image, styles: { medium: "475x325>", thumb: "350x250>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
