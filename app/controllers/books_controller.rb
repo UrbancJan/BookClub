d670bfe2e83243d7efc581d36f419c527a8fcf8e
lord-kappa <jan.urbanc4@gmail.com>
2018-03-05 18:24:08 +0100

    majhne spremembe

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.order(created_at: :desc)
   end
 
   # GET /books/1
